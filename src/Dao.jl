cf710b6dc029c25aeb84e5e21588758ccd6609a4
Gregory Wagner <wagner.greg@gmail.com>
2019-04-20 02:46:47 -0400

    Fixes syntax errors

--- src/Dao.jl
+++ src/Dao.jl
@@ -4,10 +4,9 @@ export
     markov_link,
     markov_chain,
 
-    MarkovLink, MarkovChain, LossFunction,
-
-    ColumnModel, ColumnModelData, simple_flux_model, temperature_loss
-    
+    MarkovLink,
+    MarkovChain,
+    LossFunction
 
 using
     Distributions,
@@ -18,9 +17,6 @@ using
 import Base: length
 
 include("markov.jl")
-include("file_wrangling.jl")
-include("column_models.jl")
-include("kpp_utils.jl")
 
 """
     markov_link(loss, param, error, error_scale, perturb)
@@ -78,26 +74,22 @@ function markov_chain_extra(loss, init_param, perturb, error_scale, nt)
     return param, error, test_param, test_error
 end
 
-<<<<<<< HEAD
-function markov_chain_with_save(loss, init_param, perturb, error_scale, nt, filename)
-=======
 
 function markov_chain_with_save(loss, init_param, perturb, error_scale, nt, filename,freq)
->>>>>>> 756d34984d4adca5a239619dcd8007332216ff90
-    param = ones(length(init_param),nt+1)
-    @views @. param[:,1] = init_param
+    param = ones(length(init_param), nt+1)
+    @views @. param[:, 1] = init_param
     test_param = deepcopy(param)
     error = ones(nt+1) .* 10^6
     test_error = deepcopy(error)
     error[1] = loss(init_param)
     for i in 1:nt
-        new_param, new_error, proposal_param, proposal_error = markov_link(loss, param[:,i], error[i], error_scale\
-, perturb)
-        @views @. param[:,i+1] = new_param
+        new_param, new_error, proposal_param, proposal_error = markov_link(loss, param[:, i], error[i], error_scale,
+                                                                           perturb)
+        @views @. param[:, i+1] = new_param
         error[i+1] = new_error
-        @views @. test_param[:,i+1] = proposal_param
+        @views @. test_param[:, i+1] = proposal_param
         test_error[i+1] = proposal_error
-        if i%freq==0
+        if i % freq == 0
         println("saving index "*string(i))
         @save filename error param
         end
