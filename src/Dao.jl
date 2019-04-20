6ae4da93bcd7e0d268bb2422ee6538c495e4a71f
Gregory Wagner <wagner.greg@gmail.com>
2019-04-20 15:18:17 -0400

    Formatting improvements -- spaces after commas, vertical whitespace, comments

--- src/Dao.jl
+++ src/Dao.jl
@@ -39,18 +39,21 @@ function markov_link(loss, param, error, error_scale, perturb)
     return new_param, new_error, test_param, test_error
 end
 
-#(loss::Function, init_param::Array{Float64,1}, perturb::Function, error_scale::Float64, nt::Int64)
 function markov_chain(loss, init_param, perturb, error_scale, nt)
     param = ones(length(init_param), nt+1)
     @views @. param[:, 1] = init_param
+
     test_param = deepcopy(param)
     error = ones(nt+1) .* 10^6
+
     test_error = deepcopy(error)
     error[1] = loss(init_param)
     for i in 1:nt
         new_param, new_error, proposal_param, proposal_error = markov_link(loss, param[:, i], error[i], error_scale, perturb)
+
         @views @. param[:, i+1] = new_param
         error[i+1] = new_error
+
         @views @. test_param[:, i+1] = proposal_param
         test_error[i+1] = proposal_error
     end
@@ -60,17 +63,24 @@ end
 function markov_chain_extra(loss, init_param, perturb, error_scale, nt)
     param = ones(length(init_param),nt+1)
     @views @. param[:,1] = init_param
+
     test_param = deepcopy(param)
     error = ones(nt+1) .* 10^6
+
     test_error = deepcopy(error)
     error[1] = loss(init_param)
+
     for i in 1:nt
-        new_param, new_error, proposal_param, proposal_error = markov_link(loss, param[:,i], error[i], error_scale, perturb)
+        new_param, new_error, proposal_param, proposal_error = markov_link(
+            loss, param[:, i], error[i], error_scale, perturb)
+
         @views @. param[:,i+1] = new_param
         error[i+1] = new_error
+
         @views @. test_param[:,i+1] = proposal_aram
         test_error[i+1] = proposal_error
     end
+
     return param, error, test_param, test_error
 end
 
@@ -78,20 +88,27 @@ end
 function markov_chain_with_save(loss, init_param, perturb, error_scale, nt, filename,freq)
     param = ones(length(init_param), nt+1)
     @views @. param[:, 1] = init_param
+
+    # Initialization
     test_param = deepcopy(param)
     error = ones(nt+1) .* 10^6
     test_error = deepcopy(error)
     error[1] = loss(init_param)
+
+    # Chain
     for i in 1:nt
-        new_param, new_error, proposal_param, proposal_error = markov_link(loss, param[:, i], error[i], error_scale,
-                                                                           perturb)
+        new_param, new_error, proposal_param, proposal_error = markov_link(
+            loss, param[:, i], error[i], error_scale, perturb)
+
         @views @. param[:, i+1] = new_param
         error[i+1] = new_error
+
         @views @. test_param[:, i+1] = proposal_param
         test_error[i+1] = proposal_error
+
         if i % freq == 0
-        println("saving index "*string(i))
-        @save filename error param
+            println("saving index "*string(i))
+            @save filename error param
         end
     end
     return param, error
