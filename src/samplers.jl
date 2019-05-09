894d2bc394598c19d8d0b61bbb60b4c94fae89e7
Gregory Wagner <wagner.greg@gmail.com>
2019-05-09 06:05:13 -0400

    Adds a bounded normal perturbation

--- src/samplers.jl
+++ src/samplers.jl
@@ -15,12 +15,26 @@ struct NormalPerturbation{T}
     std :: T
 end
 
+struct BoundedNormalPerturbation{T, B}
+    std :: T
+    bounds :: B
+end
+
 (pert::NormalPerturbation)(θ) = normal_perturbation(θ, pert.std)
+(pert::BoundedNormalPerturbation)(θ) = bounded_normal_perturbation(θ, pert.std, pert.bounds)
 
 function normal_perturbation(θ::AbstractArray, std)
     θ′ = similar(θ)
     for i in eachindex(θ′)
         @inbounds θ′[i] = θ[i] + rand(Normal(0, std[i]))
+    end
+    return θ′
+end
+
+function bounded_normal_perturbation(θ, std, bounds)
+    θ′ = similar(θ)
+    for i in eachindex(θ′)
+        @inbounds θ′[i] = torus(θ[i] + rand(Normal(0, std[i])), bounds[i][1], bounds[i][2])
     end
     return θ′
 end
